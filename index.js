ba6af839747d2012335b576840a95bb742cabec9
olumild <olumild998@gmail.com>
2023-08-28 15:49:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const btnEl=document.querySelector('.btn')
+const spanEl=document.getElementById('score')
+
+
+function increment(){
+    spanEl.innerText++    // spanEl.innerText=spanEl.innerText + 1
+
+}
+
